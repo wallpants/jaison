63dc539b0730cf9fe551b4f57de6ec3165893873
wallpants <47203170+wallpants@users.noreply.github.com>
2024-05-17 00:43:06 -0600

    feat(database): setup database

--- app/schemas/database/users.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-
-export const usersTable = pgTable("users", {
-   id: serial("id").primaryKey(),
-   firstname: text("firstname"),
-   lastname: text("lastname"),
-   phone: varchar("phone", { length: 256 }),
-});
