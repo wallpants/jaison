fb3582b1bebc292cd6faa4f229432df561b722f5
wallpants <47203170+wallpants@users.noreply.github.com>
2024-05-18 22:01:40 -0600

    fix: logo redirect to dashboard if user is signed in

--- app/routes/_index/top-bar.tsx
+++ app/routes/_index/top-bar.tsx
@@ -1,33 +1,29 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
-import { Link, useLocation } from "@remix-run/react";
+import { Link } from "@remix-run/react";
 
 type Props = {
    userEmail: string | undefined;
 };
 
-export const TopBar = ({ userEmail }: Props) => {
-   const location = useLocation();
-
-   return (
-      <div className="flex h-12 shrink-0 items-center justify-between border-b-2 px-4 py-2">
-         <Link to={location.pathname.startsWith("/dashboard") ? "/dashboard" : "/"}>
-            <h2 className="ml-10 text-xl font-bold md:ml-0">
-               J<span className="text-primary">ai</span>son
-            </h2>
-         </Link>
-         <div className="flex items-center">
-            {userEmail ? (
-               <Button variant="link" asChild>
-                  <Link to="/dashboard">{userEmail}</Link>
-               </Button>
-            ) : (
-               <Button variant="link" asChild>
-                  <Link to="/login">Login</Link>
-               </Button>
-            )}
-            <ModeToggle />
-         </div>
+export const TopBar = ({ userEmail }: Props) => (
+   <div className="flex h-12 shrink-0 items-center justify-between border-b-2 px-4 py-2">
+      <Link to={userEmail ? "/dashboard" : "/"}>
+         <h2 className="ml-10 text-xl font-bold md:ml-0">
+            J<span className="text-primary">ai</span>son
+         </h2>
+      </Link>
+      <div className="flex items-center">
+         {userEmail ? (
+            <Button variant="link" asChild>
+               <Link to="/dashboard">{userEmail}</Link>
+            </Button>
+         ) : (
+            <Button variant="link" asChild>
+               <Link to="/login">Login</Link>
+            </Button>
+         )}
+         <ModeToggle />
       </div>
-   );
-};
+   </div>
+);
