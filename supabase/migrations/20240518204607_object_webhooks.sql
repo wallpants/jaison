80ad811831cd25827f61796690c2dec2411a9abf
wallpants <47203170+wallpants@users.noreply.github.com>
2024-05-19 16:16:10 -0600

    refactor(migrations): squash migrations to single file

--- supabase/migrations/20240518204607_object_webhooks.sql
+++
@@ -1,9 +0,0 @@
-create trigger "insert_object" after insert
-on "storage"."objects" for each row
-execute function "supabase_functions"."http_request"(
-  'http://host.docker.internal:5173/objects/webhook',
-  'POST',
-  '{"Content-Type":"application/json"}',
-  '{}',
-  '5000'
-);
