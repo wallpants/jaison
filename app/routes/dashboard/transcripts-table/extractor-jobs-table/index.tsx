d455fe76ba447297032dc6fe6182fd3ab79c1b7a
wallpants <47203170+wallpants@users.noreply.github.com>
2024-05-18 22:33:09 -0600

    fix(dashboard): fix table updates

--- app/routes/dashboard/transcripts-table/extractor-jobs-table/index.tsx
+++ app/routes/dashboard/transcripts-table/extractor-jobs-table/index.tsx
@@ -1,10 +1,16 @@
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { useTable } from "@/lib/use-table";
+import { SerializeFrom } from "@remix-run/node";
+import { useRouteLoaderData } from "@remix-run/react";
+import { loader } from "../../route";
 import { Row, columns } from "./columns";
 
-export const ExtractorJobsTable = ({ extractorJobs }: { extractorJobs: Row[] }) => {
+export const ExtractorJobsTable = ({ transcriptId }: { transcriptId: number }) => {
+   const loaderData = useRouteLoaderData<SerializeFrom<typeof loader>>("routes/dashboard");
+   const transcript = loaderData?.transcripts.find((t) => t.id === transcriptId);
+
    const table = useTable<Row>({
-      rows: extractorJobs,
+      rows: transcript?.extractor_jobs ?? [],
       columns,
    });
 
